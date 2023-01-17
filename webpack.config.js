eaf9f063221ad7f8ccbfd2c41d85c2f8d0ccd68e
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-17 17:30:41 -0400

    add header and input field

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/assets/scripts/index.js',
   output: {
     filename: '[name].js',
